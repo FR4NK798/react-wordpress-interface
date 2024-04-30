fae6a9ffa7847b38c056a79f015c6922e761cd44
FR4NK798 <frankricciardi98@gmail.com>
2024-04-30 23:31:25 +0200

    commit

--- src/components/AddPost.jsx
+++ src/components/AddPost.jsx
@@ -5,34 +5,63 @@ import { authString } from "../constants.js";
 // import { Link } from "react-router-dom/dist";
 import Form from "react-bootstrap/Form";
 import Button from "react-bootstrap/Button";
+import InputGroup from "react-bootstrap/InputGroup";
 
 const AddPost = () => {
-  //   const [posts, setPosts] = useState([]);
-  //   const [form, setForm] = useState(null);
   const [newTitle, setNewTitle] = useState("");
   const [newContent, setNewContent] = useState("");
-  //   const [currentPage, setCurrentPage] = useState(1);
+
+  const [decision, setDecision] = useState("articolo");
 
   const addFromForm = (e) => {
-    // if (setNewTitle !== "" && newContent !== "") {
     e.preventDefault();
-    fetch(`${baseApiUrl}/posts`, {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-        Authorization: `Basic ${authString}`,
-      },
-      body: JSON.stringify({
-        title: newTitle,
-        content: newContent,
-        status: "publish",
-      }),
-    });
+    console.log("decision dopo", decision);
+    if (decision === "articolo") {
+      fetch(`${baseApiUrl}/posts`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Basic ${authString}`,
+        },
+        body: JSON.stringify({
+          title: newTitle,
+          content: newContent,
+          status: "publish",
+        }),
+      });
+    } else {
+      fetch(`${baseApiPage}`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Basic ${authString}`,
+        },
+        body: JSON.stringify({
+          title: newTitle,
+          content: newContent,
+          status: "publish",
+        }),
+      });
+    }
   };
 
   return (
     <>
       <Form onSubmit={addFromForm}>
+        <Form.Group className="mb-2">
+          <Form.Label>Articolo o pagina</Form.Label>
+          <Form.Control
+            as="select"
+            value={decision}
+            onChange={(e) => {
+              setDecision(e.target.value);
+            }}
+          >
+            <option value="articolo">Articolo</option>
+            <option value="pagina">Pagina</option>
+          </Form.Control>
+        </Form.Group>
+
         <Form.Group className="mb-2">
           <Form.Label>Nuovo titolo</Form.Label>
           <Form.Control
@@ -56,8 +85,6 @@ const AddPost = () => {
         </Form.Group>
 
         <Button variant="primary" type="submit">
-          {/* {console.log(newTitle)}
-          {console.log(newContent)} */}
           Invia
         </Button>
       </Form>
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -3,43 +3,34 @@ import { baseApiUrl } from "../constants.js";
 import { baseApiPage } from "../constants.js";
 import { Link } from "react-router-dom/dist";
 
-
 import { authString } from "../constants.js";
 
-import Row from 'react-bootstrap/Row'
-import Col from 'react-bootstrap/Col'
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
 
 const Home = () => {
-    // articoli
+  // articoli
   const [posts, setPosts] = useState([]);
   const [lastPage, setLastPage] = useState(null);
   const [currentPage, setCurrentPage] = useState(1);
   const [deleteState, setDeleteState] = useState(null);
 
-//   pagine
-const [pages, setPages] = useState([]);
-const [lastPagePages, setLastPagePages] = useState(null);
-const [currentPagePages, setCurrentPagePages] = useState(1);
-const [deleteStatePages, setDeleteStatePages] = useState(null);
-
-
-
-
-
-
-
-
+  //   pagine
+  const [pages, setPages] = useState([]);
+  const [lastPagePages, setLastPagePages] = useState(null);
+  const [currentPagePages, setCurrentPagePages] = useState(1);
+  const [deleteStatePages, setDeleteStatePages] = useState(null);
 
   // fetch pagine
   useEffect(() => {
-    fetch(`${baseApiPage}/?page=${currentPage}`)
+    fetch(`${baseApiPage}/?page=${currentPagePages}`)
       .then((res) => {
         // recupera i dati della paginazione dagli header
         setLastPage(parseInt(res.headers.get("X-WP-TotalPages")));
         return res.json();
       })
       .then((data) => {
-        console.log('pagine',data);
+        console.log("pagine", data);
         setPages(data);
       });
 
@@ -51,13 +42,12 @@ const [deleteStatePages, setDeleteStatePages] = useState(null);
         return res.json();
       })
       .then((data) => {
-        console.log('post', data);
+        console.log("post", data);
         setPosts(data);
       });
   }, [currentPage, deleteState, currentPagePages, deleteStatePages]);
 
-
-//   article
+  //   article
   const changePage = (page) => {
     setCurrentPage(page);
   };
@@ -73,41 +63,37 @@ const [deleteStatePages, setDeleteStatePages] = useState(null);
     return paginationArr;
   };
 
-
-//   pages
-const changePagePages = (page) => {
+  //   pages
+  const changePagePages = (page) => {
     setCurrentPagePages(page);
   };
 
   const generatePaginationArrayPages = () => {
-    let paginationArr = [];
-    for (let index = 1; index <= lastPage; index++) {
-      paginationArr.push({
+    let paginationArrPages = [];
+    for (let index = 1; index <= lastPagePages; index++) {
+      paginationArrPages.push({
         n: index,
-        active: currentPage === index,
+        active: currentPagePages === index,
       });
     }
-    return paginationArr;
+    return paginationArrPages;
   };
 
-
-
-
-    // elimina post
-    const deletePost = (id) => {
-      fetch(`${baseApiUrl}/posts/${id}`, {
-        method: "DELETE",
-        headers: {
-          "Content-Type": "application/json",
-          Authorization: `Basic ${authString}`,
-        },
-      })
-        .then((res) => res.json())
-        .then((data) => {
-          // console.log(data)
-          setDeleteState(true);
-        });
-    };
+  // elimina post
+  const deletePost = (id) => {
+    fetch(`${baseApiUrl}/posts/${id}`, {
+      method: "DELETE",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Basic ${authString}`,
+      },
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        // console.log(data)
+        setDeleteState(true);
+      });
+  };
 
   // elimina pagina
   const deletePages = (id) => {
@@ -127,50 +113,105 @@ const changePagePages = (page) => {
 
   return (
     <>
-    <Row>
+      <Row>
         <Col>
-      <ul>
-        {posts.map((post) => (
-          <li key={post.id}>
-            <Link to={`/posts/${post.id}`}>{post.title.rendered}</Link>
-            <button onClick={() => deletePost(post.id)}>Elimina</button>
-            <Link to={`/edit/${post.id}`}>Modifica</Link>
-            {/* <button onClick={() => deletePost(post.id)}>Modifica</button> */}
-          </li>
-        ))}
-      </ul>
+          <ul>
+            {posts.map((post) => (
+              <li key={post.id}>
+                <Link to={`/posts/${post.id}`}>{post.title.rendered}</Link>
+                <button onClick={() => deletePost(post.id)}>Elimina</button>
+                <Link to={`/edit/${post.id}`}>Modifica</Link>
+              </li>
+            ))}
+          </ul>
 
-      <ul className="pagination">
-        <li className={`page-item ${currentPage === 1 && "disabled"}`}>
-          <span
-            className="page-link"
-            onClick={() => currentPage !== 1 && changePage(currentPage - 1)}
-          >
-            Previous
-          </span>
-        </li>
+          <ul className="pagination">
+            <li className={`page-item ${currentPage === 1 && "disabled"}`}>
+              <span
+                className="page-link"
+                onClick={() => currentPage !== 1 && changePage(currentPage - 1)}
+              >
+                Previous
+              </span>
+            </li>
 
-        {generatePaginationArray().map((page) => (
-          <li key={page.n} className={`page-item ${page.active && "active"}`}>
-            <span className="page-link" onClick={() => changePage(page.n)}>
-              {page.n}
-            </span>
-          </li>
-        ))}
+            {generatePaginationArray().map((page) => (
+              <li
+                key={page.n}
+                className={`page-item ${page.active && "active"}`}
+              >
+                <span className="page-link" onClick={() => changePage(page.n)}>
+                  {page.n}
+                </span>
+              </li>
+            ))}
 
-        <li className={`page-item ${currentPage === "lastPage" && "disabled"}`}>
-          <span
-            className="page-link"
-            onClick={() =>
-              currentPage !== lastPage && changePage(currentPage + 1)
-            }
-          >
-            Next
-          </span>
-        </li>
-      </ul>
+            <li
+              className={`page-item ${
+                currentPage === "lastPage" && "disabled"
+              }`}
+            >
+              <span
+                className="page-link"
+                onClick={() =>
+                  currentPage !== lastPage && changePage(currentPage + 1)
+                }
+              >
+                Next
+              </span>
+            </li>
+          </ul>
+        </Col>
+      </Row>
+      <Row>
+        <Col>
+        <ul>
+            {pages.map((pagina) => (
+              <li key={pagina.id}>
+                <Link to={`/posts/${pagina.id}`}>{pagina.title.rendered}</Link>
+                <button onClick={() => deletePages(pagina.id)}>Elimina</button>
+                <Link to={`/edit/${pagina.id}`}>Modifica</Link>
+              </li>
+            ))}
+          </ul>
 
-      </Col>
+          <ul className="pagination">
+            <li className={`page-item ${currentPagePages === 1 && "disabled"}`}>
+              <span
+                className="page-link"
+                onClick={() => currentPagePages !== 1 && changePagePages(currentPagePages - 1)}
+              >
+                Previous
+              </span>
+            </li>
+
+            {generatePaginationArrayPages().map((page) => (
+              <li
+                key={page.n}
+                className={`page-item ${page.active && "active"}`}
+              >
+                <span className="page-link" onClick={() => changePagePages(page.n)}>
+                  {page.n}
+                </span>
+              </li>
+            ))}
+
+            <li
+              className={`page-item ${
+                currentPagePages === "lastPage" && "disabled"
+              }`}
+            >
+              <span
+                className="page-link"
+                onClick={() =>
+                  currentPagePages !== lastPagePages && changePagePages(currentPagePages + 1)
+                }
+              >
+                Next
+              </span>
+            </li>
+          </ul>
+        </Col>
       </Row>
     </>
   );
